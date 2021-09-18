9cc7f6170d27ebc7fbfbf39752bd603b99790fac
Alexander mokhov <wannasludge@yandex.ru>
2021-09-18 13:16:13 +0300

    Update code

--- BmiService.java
+++ BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double mass, double high) {
-        double index = mass / Math.pow(high,2) * 10000;
+        double index = mass / Math.pow(high, 2) * 10000;
         return index;
     }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-BmiService service = new BmiService();
-double index = service.calculate(80, 176);
-System.out.printf("%.1f",index);
+        BmiService service = new BmiService();
+        double index = service.calculate(80, 176);
+        System.out.printf("%.1f", index);
 // мне показалось, что если выводить индекс в целых числах,
 // то результат будет не совсем точным, однако простыню знаков
 // после запятой мне тоже не особо хотелось выводить
